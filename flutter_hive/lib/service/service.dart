b3a92c27934c4866bd3eefc72f878a00c0c68efe
mensur056 <mansur.sarxanov@gmail.com>
2023-02-16 17:10:40 +0400

    test

---
+++ flutter_hive/lib/service/service.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_hive/service/user_model.dart';
+
+abstract class IService {
+  Future<List<UserModel>?> fetchItemFromApi();
+}
+
+class GeneralService implements IService {
+  Dio dio = Dio(BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com/'));
+  @override
+  Future<List<UserModel>?> fetchItemFromApi() async {
+    final response = await dio.get('users');
+    if (response.statusCode == 200) {
+      final jsonData = response.data;
+      if (jsonData is List) {
+        jsonData.map((e) => UserModel.fromJson(e)).toList();
+      }
+    }
+    return null;
+  }
+}
